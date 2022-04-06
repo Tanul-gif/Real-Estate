5059d50acb723fcff325334ad8dc72a5fbf165f2
Tanulkhatri <71933192+Tanul-gif@users.noreply.github.com>
2022-04-07 00:22:56 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// window.onload = function(){ 
+//     document.getElementById("video1").addEventListener("ended", function(){ playVideo("video2"); });
+//     document.getElementById("video2").addEventListener("ended", function(){ playVideo("video3"); });
+//     document.getElementById("video3").addEventListener("ended", function(){ playVideo("video4"); });
+//     document.getElementById("video4").addEventListener("ended", function(){ playVideo("video5"); });
+//     document.getElementById("video5").addEventListener("ended", function(){ playVideo("video1"); });
+// }
+
+// function playVideo(videoID){
+//     var videoElement = document.getElementById(videoID);
+//     videoElement.play();
+// }
